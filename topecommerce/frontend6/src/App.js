a96929329246471e6fda9e4b3cedf538c0bd2516
aloy <ogechialo@ymail.com>
2022-09-15 15:46:43 +0100

    Video-03-Create-React-App

---
+++ topecommerce/frontend6/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>topecommerce</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
